ca83966163c27aab44f3db3760fa92ee57b25a77
Timo Clasen <goebeltimo@gmail.com>
2023-01-25 22:24:45 +0100

    Update Dependencies

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,38 +1,41 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const pages = defineCollection({
-  schema: {
+  schema: z.object({
     title: z.string(),
-  }
+  }),
 });
 
 const principles = defineCollection({
-  schema: {
+  schema: z.object({
     title: z.string(),
-  }
+  }),
 });
 
 const team = defineCollection({
-  schema: {
+  schema: z.object({
     name: z.string(),
-    links: z.array(z.object({
-      name: z.string(),
-      icon: z.string(),
-      url: z.string().url()
-    })).optional(),
-
-  }
+    links: z
+      .array(
+        z.object({
+          name: z.string(),
+          icon: z.string(),
+          url: z.string().url(),
+        })
+      )
+      .optional(),
+  }),
 });
 
 const friends = defineCollection({
-  schema: {
+  schema: z.object({
     name: z.string(),
-  }
+  }),
 });
 
 export const collections = {
   pages,
   principles,
   team,
-  friends
+  friends,
 };
