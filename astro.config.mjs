dae62ee635be046fb01ea6365876ae42107eecde
Timo Clasen <goebeltimo@gmail.com>
2024-01-21 21:25:48 +0100

    Update Dependencies (#6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://lifecentereddesign.co",
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), sitemap(), icon()],
 });
