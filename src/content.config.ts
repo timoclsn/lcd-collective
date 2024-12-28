8bc0d4e342a7fa4f9dc5b3013a5be098849727ff
Timo Clasen <goebeltimo@gmail.com>
2024-12-28 21:24:22 +0100

    Update dependencies
    - Astro 5.0

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,22 @@
+import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
 const pages = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/pages" }),
   schema: z.object({
     title: z.string(),
   }),
 });
 
 const principles = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/principles" }),
   schema: z.object({
     title: z.string(),
   }),
 });
 
 const team = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/team" }),
   schema: z.object({
     name: z.string(),
     links: z
@@ -21,13 +25,14 @@ const team = defineCollection({
           name: z.string(),
           icon: z.string(),
           url: z.string().url(),
-        })
+        }),
       )
       .optional(),
   }),
 });
 
 const friends = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/team" }),
   schema: z.object({
     name: z.string(),
     links: z
@@ -36,7 +41,7 @@ const friends = defineCollection({
           name: z.string(),
           icon: z.string(),
           url: z.string().url(),
-        })
+        }),
       )
       .optional(),
   }),
