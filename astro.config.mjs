311e82d5fc9c797d234b52c9aa5a7f63fb03ed91
Timo Clasen <goebeltimo@gmail.com>
2025-02-16 20:08:24 +0100

    Upgrade astro and start tailwind upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://lifecentereddesign.co",
   integrations: [tailwind(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
