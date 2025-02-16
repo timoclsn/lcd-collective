f127f1925a3d2241aa8cc8e459ed36d5963dc89e
Timo Clasen <goebeltimo@gmail.com>
2025-02-16 20:17:32 +0100

    Finish upgrading tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
   site: "https://lifecentereddesign.co",
-  integrations: [tailwind(), sitemap(), icon()],
-
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
